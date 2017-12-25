0ced7a8fa2eb18154370ec98acb2c89d9279a206
dimotim <datdimotim@gmail.com>
2017-12-25 19:02:22 +0300

    RELEASE 2.1 new tests more added reformed tests design TODO: analyze results

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,87 +6,83 @@ import com.dimotim.huffman.StaticHuffmanAlgorithm;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        for(File f:new File("tests").listFiles()) {
-            System.out.println("file "+f.getName()+"  ");
-            compareLength("tests/"+f.getName());
-            System.out.println();
-        }
-        //test();
-        //benchmarkStatic();
-        //benchmarkAdaptive();
+        System.out.println("TESTS");
+        applyOnSet("tests", new Tester() {
+            @Override
+            public void process(String file) throws Exception {
+                System.out.print("\tstatic: ");
+                test(StaticHuffmanAlgorithm::encode,StaticHuffmanAlgorithm::decode,file);
+                System.out.print("\tadaptive: ");
+                test(AdaptiveHuffmanAlgorithm::encode,AdaptiveHuffmanAlgorithm::decode,file);
+            }
+        });
+
+        System.out.println("\nCOMPARE LENGTH");
+        applyOnSet("tests", Test::compareLength);
+
+        System.out.println("\nBENCHMARKS");
+        System.out.println("static");
+        benchmark("tests/vim 30M",StaticHuffmanAlgorithm::encode,StaticHuffmanAlgorithm::decode,10);
+        System.out.println("\nadaptive");
+        benchmark("tests/vim 30M",AdaptiveHuffmanAlgorithm::encode,AdaptiveHuffmanAlgorithm::decode,10);
     }
+
     public static void compareLength(String name) throws Exception {
         ByteArrayOutputStream baos=new ByteArrayOutputStream();
-        StaticHuffmanAlgorithm.encode(new FileInputStream(name),baos);
-        System.out.println("static "+baos.toByteArray().length);//18262224
+        byte[] input=new FileInputStream(name).readAllBytes();
+        StaticHuffmanAlgorithm.encode(new ByteArrayInputStream(input),baos);
+        System.out.println("\traw file "+input.length);
+        System.out.println("\tstatic "+baos.toByteArray().length);//18262224
 
         baos.reset();
-        AdaptiveHuffmanAlgorithm.encode(new ByteArrayInputStream(new FileInputStream(name).readAllBytes()),baos);
-        System.out.println("adaptive "+baos.toByteArray().length);
-    }
-    public static void test() throws Exception {
-        System.out.println("static");
-        testSet(StaticHuffmanAlgorithm::encode,StaticHuffmanAlgorithm::decode,new File("tests").listFiles());
-        System.out.println("\nadaptive:");
-        testSet(AdaptiveHuffmanAlgorithm::encode,AdaptiveHuffmanAlgorithm::decode,new File("tests").listFiles());
-        System.out.println();
-    }
-    public static void benchmarkStatic() throws Exception {
-        benchmark(new FileInputStream("tests/vim 30M"),StaticHuffmanAlgorithm::encode,StaticHuffmanAlgorithm::decode);
-    }
-    public static void benchmarkAdaptive() throws Exception {
-        benchmark(new FileInputStream("tests/vim 30M"),AdaptiveHuffmanAlgorithm::encode,AdaptiveHuffmanAlgorithm::decode);
+        AdaptiveHuffmanAlgorithm.encode(new ByteArrayInputStream(input),baos);
+        System.out.println("\tadaptive "+baos.toByteArray().length);
     }
 
-    public static void testSet(Encoder encoder, Decoder decoder, File... files) throws Exception {
-        for(File fileName:files) {
-            FileInputStream file=new FileInputStream(fileName);
-            byte[] fileBytes = file.readAllBytes();
-            file.close();
+    public static void test(Encoder encoder, Decoder decoder, String fileName) throws Exception {
+        FileInputStream file=new FileInputStream(fileName);
+        byte[] fileBytes = file.readAllBytes();
+        file.close();
 
-            ByteArrayOutputStream encoded = new ByteArrayOutputStream();
-            ByteArrayInputStream inputFile = new ByteArrayInputStream(fileBytes);
-            ByteArrayOutputStream decoded = new ByteArrayOutputStream();
+        ByteArrayOutputStream encoded = new ByteArrayOutputStream();
+        ByteArrayInputStream inputFile = new ByteArrayInputStream(fileBytes);
+        ByteArrayOutputStream decoded = new ByteArrayOutputStream();
 
-            encoder.encode(inputFile, encoded);
-            ByteArrayInputStream inputCode = new ByteArrayInputStream(encoded.toByteArray());
-            decoder.decode(inputCode, decoded);
-            if (!Arrays.equals(fileBytes, decoded.toByteArray())) throw new RuntimeException("file: "+fileName+" unmatch!");
-            else System.out.println("file: "+fileName+" ok");
+        encoder.encode(inputFile, encoded);
+        ByteArrayInputStream inputCode = new ByteArrayInputStream(encoded.toByteArray());
+        decoder.decode(inputCode, decoded);
+        if (!Arrays.equals(fileBytes, decoded.toByteArray())) throw new RuntimeException("unmatch!");
+        else System.out.println("ok");
+    }
+
+    public static void applyOnSet(String folder, Tester tester) throws Exception {
+        for (File f:new File(folder).listFiles()){
+            System.out.println("file: "+f.getName());
+            tester.process(folder+"/"+f.getName());
+            System.out.println();
         }
     }
-    public static void testPriorityQueue(){
-        Random r=new Random();
-        int[] m=new int[10];
-        for(int i=0;i<m.length;i++)m[i]=r.nextInt(4);
-        PriorityQueue<Integer> pq=new PriorityQueue<>(m.length,Integer[]::new);
-        for(int i=0;i<m.length;i++)pq.push(m[i]);
-
-        Integer prev=pq.pop();
-        System.out.println("prior="+prev);
-        while (0!=pq.size()){
-            Integer cur=pq.pop();
-            System.out.println("prior="+cur);
-        }
+    public interface Tester{
+        void process(String file) throws Exception;
     }
-
     public interface Encoder{
         void encode(InputStream in, OutputStream out)throws Exception;
     }
     public interface Decoder{
         void decode(InputStream in, OutputStream out)throws IOException;
     }
-    public static void benchmark(InputStream file, Encoder encoder, Decoder decoder) throws Exception {
-        byte[] fileBytes=file.readAllBytes();
+    public static void benchmark(String file, Encoder encoder, Decoder decoder, int count) throws Exception {
+        InputStream file_f=new FileInputStream(file);
+        byte[] fileBytes=file_f.readAllBytes();
+        file_f.close();
         ByteArrayOutputStream encoded=new ByteArrayOutputStream();
         ByteArrayInputStream inputFile=new ByteArrayInputStream(fileBytes);
         ByteArrayOutputStream decoded=new ByteArrayOutputStream();
 
-        while (true) {
+        for(int i=0;i<count;i++) {
             encoded.reset();
             inputFile.reset();
             decoded.reset();
